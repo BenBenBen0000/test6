f50428c047403ab7dcd71ae3b1a42859009cb02a
Ben <xxswhy109@163.com>
2021-09-06 20:39:07 +0800

    t1

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Ben
+ * @Date: 2021/9/5 19:59
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World1231245645678");
+    }
+}
